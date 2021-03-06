0275b7935612153b272e609994e8f5087dea54b6
Nick <74734381+nickstillman@users.noreply.github.com>
2021-03-06 17:43:46 -0500

    Basic server (#4)
    * test branch commit
    
    * working on getRecipes functionality, CRUD mostly done
    
    Co-authored-by: Hanji Chen <77425712+whyWhyDev@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
+
 const mongoose = require('mongoose');
 const ItemController = require('./dbController')
 
 
 const app = express();
+
+const apiRouter = require('./routes/api');
+
 const PORT = 3000;
 
 mongoose.connect('mongodb+srv://ziyadelbaz:PYkNVJqtfCs8zDnk@cluster0.bafl3.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -34,11 +39,39 @@ app.patch('/:id', ItemController.updateItem)
 
 
 app.use('/build', express.static(path.join(__dirname, '../build')));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
+
+
+
+app.use('/api', apiRouter)
+
+
+
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('Unknown route!'));
+
+/**
+ * express error handler
+ * @see https://expressjs.com/en/guide/error-handling.html#writing-error-handlers
+ */
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 
 /**
  * START SERVER
