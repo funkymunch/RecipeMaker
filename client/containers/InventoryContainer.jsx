8f4b1303bdc3096e1972abcb3ddce418e6efc986
WhyWhyDev <WhyWhyDev@gmail.com>
2021-03-08 14:55:25 -0500

    midday merge

--- client/containers/InventoryContainer.jsx
+++ client/containers/InventoryContainer.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import InventoryBucket from '../components/InventoryBucket';
 import { useInventory, useSetInventory } from '../contexts/InventoryContexts';
 
@@ -8,12 +9,13 @@ function InventoryContainer() {
 
   // this works like componentDidMount and willMount
   // this is will give us the initial state of the inventory
-  // useEffect(() => {
-  //   axios.get('./api/inventory').then((res) => {
-  //     // this is a sample data structure from the server: {carrot: {itemName: carrot: bucketNumber: 1, use: true, _id: 1234}}
-  //     setInventory(res.data);
-  //   });
-  // }, []);
+  useEffect(() => {
+    axios.get('./api/inventory').then((res) => {
+      // this is a sample data structure from the server: {carrot: {itemName: carrot: bucketNumber: 1, use: true, _id: 1234}}
+      //                                                  [{itemName: carrot: bucketNumber: 1, use: true, _id: 1234}]
+      setInventory(res.data);
+    });
+  }, []);
 
   if (inventory === null) {
     return 'Loading...';
@@ -31,9 +33,14 @@ function InventoryContainer() {
     InventoryBuckets.push(<InventoryBucket key={`ib${i}`} bucket={bucket} bucketNumber={i} />);
   }
 
+  function getRecipes() {}
+
   return (
     // three buckets with data passed down for each bucket
-    <div>{InventoryBuckets}</div>
+    <div>
+      {InventoryBuckets}
+      <button onClick={null}>Submit</button>
+    </div>
   );
 }
 
